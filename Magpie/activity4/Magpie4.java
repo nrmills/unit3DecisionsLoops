254dd4d6fd47887a0cea37a5688e18f6bd8bf53e
Nicolas MIlls <millspec98@yahoo.com>
2014-10-22 12:21:38 -0500

    magpie4

--- Magpie/activity4/Magpie4.java
+++ Magpie/activity4/Magpie4.java
@@ -31,6 +31,7 @@ public class Magpie4
 	public String getResponse(String statement)
 	{
 		String response = "";
+		statement = statement.trim().toLowerCase();
 		if (statement.length() == 0)
 		{
 			response = "Say something, please.";
@@ -94,9 +95,30 @@ public class Magpie4
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
 	}
+	
+	/**
+	 * Take a statement with "I want to <something>." and transform it into 
+	 * "What would it mean to <something>?"
+	 * @param statement the user statement, assumed to contain "I want to"
+	 * @return the transformed statement
+	 */
+	private String transformIWantStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "What would it mean to " + restOfStatement + "?";
+	}
 
-	
-	
+
 	/**
 	 * Take a statement with "you <something> me" and transform it into 
 	 * "What makes you think that I <something> you?"
