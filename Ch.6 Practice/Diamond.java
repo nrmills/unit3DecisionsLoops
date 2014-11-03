61e5dd11a65db390ed37899b2f8cd38eb379d123
Nicolas MIlls <millspec98@yahoo.com>
2014-11-03 12:20:25 -0600

    Finished Diamond

--- Ch.6 Practice/Diamond.java
+++ Ch.6 Practice/Diamond.java
@@ -4,14 +4,59 @@ public class Diamond
 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter the Side Length: ");
-        int sideLength = in.nextInt();
-        
-        for (int index=0;index<=sideLength*2-1;index++)
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many Diamonds would you like to Draw: ");
+        int iterations = input.nextInt();
+        System.out.println();
+
+        for (int index =0; index < iterations; index++)
         {
-            for (int length=1;length<=sideLength*2-1;index++)
+            int rowNum = 1;
+            int spc;
+            int y = 0;
+            int numA = 1;
+            System.out.print("Enter the Desired Width of the Diamond- Must be an Odd Number: ");
+            int width = input.nextInt()+1;
+            if (width % 2 == 0 && (width/2)%2 == 0)
             {
+                spc = (width/2)-1;
+            }
+            else
+            {
+                spc = (width-1)/2;
+            }
+
+            while(rowNum<width)
+            {
+                //Prints the spaces before the asterisks
+                for(int x=1;x<=spc;x++)
+                {
+                    System.out.print(" ");
+                }
+                //Prints asterisks
+                while(y < numA)
+                {
+                    System.out.print("*");
+                    y++;
+                }
+                //Prints the spaces after the asterisks- Not Needed, for visualisation
+                //for(int z=1;z<=spc;z++)
+                //{
+                //    System.out.print(" ");
+                //}
+                System.out.println();
+                if (rowNum >= width/2)
+                {
+                    spc += 1;
+                    numA-= 2;
+                }
+                else
+                {
+                    spc -= 1;
+                    numA += 2;
+                }
+                rowNum +=1;
+                y=0;
             }
         }
     }
