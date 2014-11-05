b7d30742e7353b71402403291681eb494aa2cbc6
Nicolas MIlls <millspec98@yahoo.com>
2014-11-05 12:46:04 -0600

    Miscellaneous Changes

--- Ch.6 Practice/Diamond.java
+++ Ch.6 Practice/Diamond.java
@@ -9,23 +9,14 @@ public class Diamond
         int iterations = input.nextInt();
         System.out.println();
 
-<<<<<<< HEAD
-        for (int index =0; index < iterations; index++)
-=======
         for (int index =0; index <= iterations; index++)
->>>>>>> origin/master
         {
             int rowNum = 1;
             int spc;
-            int y = 0;
+            int y = 1;
             int numA = 1;
-<<<<<<< HEAD
-            System.out.print("Enter the Desired Width of the Diamond- Must be an Odd Number: ");
-            int width = input.nextInt()+1;
-=======
-            System.out.print("Enter the Desired Width of the Diamond: ");
+            System.out.print("Enter the Desired Width of the Diamond (Must Be an Odd Number: ");
             int width = input.nextInt();
->>>>>>> origin/master
             if (width % 2 == 0 && (width/2)%2 == 0)
             {
                 spc = (width/2)-1;
@@ -35,32 +26,24 @@ public class Diamond
                 spc = (width-1)/2;
             }
 
-            while(rowNum<width)
+            while(rowNum<width+1)
             {
                 //Prints the spaces before the asterisks
                 for(int x=1;x<=spc;x++)
                 {
-                    System.out.print(" ");
+                    System.out.print("-");
                 }
                 //Prints asterisks
-                while(y < numA)
+                while(y <= numA)
                 {
                     System.out.print("*");
                     y++;
                 }
-<<<<<<< HEAD
-                //Prints the spaces after the asterisks- Not Needed, for visualisation
-                //for(int z=1;z<=spc;z++)
-                //{
-                //    System.out.print(" ");
-                //}
-=======
                 //Prints the spaces after the asterisks
                 for(int z=1;z<=spc;z++)
                 {
-                    System.out.print(" ");
+                    System.out.print("-");
                 }
->>>>>>> origin/master
                 System.out.println();
                 if (rowNum >= width/2)
                 {
